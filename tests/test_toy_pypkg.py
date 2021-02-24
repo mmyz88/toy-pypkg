75f492dc20e2e086020bfb638f7fd5a13fe12c06
mmyz <yzmarco.ma@gmail.com>
2021-02-24 14:00:26 -0800

    added unit tests for catbind

--- tests/test_toy_pypkg.py
+++ tests/test_toy_pypkg.py
@@ -1,5 +1,14 @@
 from toy_pypkg import __version__
 from toy_pypkg import toy_pypkg
+import pandas as pd
+
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_catbind():
+    a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+    b = pd.Categorical(["but", "integer", "where it", "counts"])
+    assert ((toy_pypkg.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
+    assert ((toy_pypkg.catbind(a, b)).categories == ["but", "character",
+            "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
